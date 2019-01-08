89874b070c041783fc2635f86f1edbbba26e1cb2
15003832047 <12345678900>
2019-01-08 11:59:28 +0800

    GitHub第一次提交

--- src/main/java/com/yunhe/cargomanagement/controller/WarehouseController.java
+++ src/main/java/com/yunhe/cargomanagement/controller/WarehouseController.java
@@ -1,9 +1,15 @@
 package com.yunhe.cargomanagement.controller;
 
 
+import com.yunhe.cargomanagement.entity.Warehouse;
+import com.yunhe.cargomanagement.service.IWarehouseService;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Map;
 
 /**
  * <p>
@@ -16,5 +22,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cargomanagement/warehouse")
 public class WarehouseController {
+
+    @Resource
+    IWarehouseService warehouseService;
+
+    /**
+     * <p>
+     *  查询所有库存信息并分页
+     * </p>
+     *
+     * @param current 当前页
+     * @param size 每页显示条数
+     * @param warehouse 实体类
+     * @return list集合
+     */
+    @RequestMapping("/getPage")
+    @ResponseBody
+    public Map selectWareHousePage(int current, int size, Warehouse warehouse){
+
+        return warehouseService.selectWareHousePage(current,size,warehouse);
+    }
+
+    @RequestMapping("/selectWareHouse")
+    @ResponseBody
+    public  Warehouse selectWareHousePage(Warehouse warehouse){
+
+        return warehouse;
+    }
 
 }
