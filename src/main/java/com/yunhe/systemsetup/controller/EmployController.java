6de4c402d7d7a6c24f2cd61670ca50d93b497c9d
13569490661 <HAO19990317>
2019-01-10 14:39:30 +0800

    没用 别合并

--- src/main/java/com/yunhe/systemsetup/controller/EmployController.java
+++ src/main/java/com/yunhe/systemsetup/controller/EmployController.java
@@ -5,6 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yunhe.systemsetup.entity.Employ;
+import com.yunhe.systemsetup.service.impl.EmployServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * <p>
  * 员工管理 前端控制器
@@ -16,5 +29,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/systemsetup/employ")
 public class EmployController {
+    @Autowired
+    private EmployServiceImpl employService;
 
+
+    @RequestMapping(value = "/selectpage")
+    public Map selectPage(Integer pageSize, Integer pageNum, Employ employ){
+
+        System.out.println(employ.getEmUsername());
+        Page page = new Page();
+        page.setSize(pageSize);
+        page.setCurrent(pageNum);
+        List<Employ> list =employService.selectPageEmploy(page,employ);
+        System.out.println(list);
+        Map map = new HashMap();
+        map.put("page",page);
+        map.put("list",list);
+        map.put("totalPage",page.getPages());
+        return map;
+    }
+
+    @RequestMapping(value = "/list")
+    public ModelAndView toList(){
+        return new ModelAndView("admin" );
+    }
+    @RequestMapping(value = "/addempl")
+    public ModelAndView toAdd(){
+
+        return new ModelAndView("admin-add");
+    }
+
+    @RequestMapping(value = "/insertempl")
+    public String inserEmploy(Employ employ){
+
+        employService.insertEmploy(employ);
+
+        return  null;
+    }
 }
