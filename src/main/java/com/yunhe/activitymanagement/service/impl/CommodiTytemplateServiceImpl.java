7d741095f5ad07f8503825b82f03b32fea6340ab
dong <ld18688826@163.com>
2019-01-14 15:12:03 +0800

    a

--- src/main/java/com/yunhe/activitymanagement/service/impl/CommodiTytemplateServiceImpl.java
+++ src/main/java/com/yunhe/activitymanagement/service/impl/CommodiTytemplateServiceImpl.java
@@ -55,6 +55,13 @@ public class CommodiTytemplateServiceImpl extends ServiceImpl<CommodiTytemplateM
         return map;
     }
 
+
+    /**
+     * <P>
+     *     查询数据
+     * </P>
+     * @return 商品模板表：查询所有模板的结果集
+     */
     @Override
     public List<CommodiTytemplate> selectAllCt() {
         return null;
