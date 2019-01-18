b998cb7d5ac90b6512930cb78e8892222b906b68
271066981@qq.com <Kzx123>
2019-01-18 20:47:31 +0800

    2019年1月18日20:47:06

--- src/main/java/com/yunhe/config/shiro/ShiroConfig.java
+++ src/main/java/com/yunhe/config/shiro/ShiroConfig.java
@@ -1,18 +1,83 @@
 package com.yunhe.config.shiro;
 
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * <per>
- *  Shiro配置类
- *  1.配置ShiroFilterFactory
- *  2.配置SecurityManager
- *  </per>
+ * Shiro配置类
+ * 1.配置ShiroFilterFactory
+ * 2.配置SecurityManager
+ * </per>
+ *
  * @author 孔邹祥
  * @date 2019年1月17日
  */
 @Configuration
 public class ShiroConfig {
 
+    /**
+     * 创建ShrioFilterFactoryBean
+     */
+    @Bean
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("securityManager") DefaultWebSecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+//            设置安全管理器
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+//        添加Shiro内置过滤器
+        /**
+         * Shior内置过滤器，可以实现权限相关的拦截器
+         *      常用拦截器
+         *          anon: 无需认证（登录）可以访问
+         *          authc:  必须认证才可以访问
+         *          user:   如果使用rememberMe的功能才可以直接访问
+         *          perms:  该资源必须得到资源权限才可以访问
+         *          pole:   该资源必须得到角色权限才可以访问
+         */
+        Map<String, String> filterMap = new LinkedHashMap<>();
+//        Shiro放行的页面
+        filterMap.put("/login", "anon");
+        filterMap.put("/", "anon");
+
+//        授权过滤器 注意: 当签授权拦截后，Shiro会自动跳转到未授权页面
+        filterMap.put("/add", "perms[user:add]");
+
+//        Shiro拦截的页面
+      /*  filterMap.put("/*", "authc");*/
+
+//        修改调整的登录页面
+        shiroFilterFactoryBean.setLoginUrl("toLogin");
+
+//        设置未授权提示页面
+        shiroFilterFactoryBean.setUnauthorizedUrl("/noAuth");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterMap);
+        return shiroFilterFactoryBean;
+    }
+
+    /**
+     * 创建DefaultWebSecurityManager
+     */
+    @Bean(name = "securityManager")
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("userRealm") UserRealm userRealm) {
+
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        //关联Realm
+        securityManager.setRealm(userRealm);
+        return securityManager;
+    }
+
+    /**
+     * 创建Realm
+     */
+    @Bean(name = "userRealm")
+    public UserRealm getRealm() {
+        return new UserRealm();
+    }
 
 }
--- src/main/java/com/yunhe/core/common/login/controller/LoginController.java
+++ src/main/java/com/yunhe/core/common/login/controller/LoginController.java
@@ -2,6 +2,13 @@ package com.yunhe.core.common.login.controller;
 
 import com.yunhe.core.common.login.service.ILoginService;
 import com.yunhe.systemsetup.entity.Employ;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -16,7 +23,7 @@ import javax.servlet.http.HttpSession;
  * @author 孔邹祥
  * @date 2019年1月9日
  */
-//@Controller
+@Controller
 public class LoginController {
 
     @Resource
@@ -26,18 +33,45 @@ public class LoginController {
     public String login() {
         return "login";
     }
-
-    @GetMapping("toindex")
+    @GetMapping("/tologin")
+    public String toLogin() {
+        return "login";
+    }
+    @GetMapping("/toindex")
     public String index() {
         return "index";
     }
-    @PostMapping("/login")
-    public String login(Employ employ, HttpSession session) {
-        if (loginService.login(employ) != null) {
-            session.setAttribute("employ", employ);
 
-            return "index";
+    @PostMapping("/login")
+    public String login(Employ employ,Model model,HttpSession session) {
+
+
+        /**
+         * 使用Shiro编写认证操作
+         */
+//        1.获取Subject
+        Subject subject = SecurityUtils.getSubject();
+
+//        2.封装用户数据
+        UsernamePasswordToken token = new UsernamePasswordToken(employ.getEmUsername(), employ.getEmPassword());
+
+//        3.执行登录方法
+        try {
+            subject.login(token);
+            session.setAttribute("employ",employ);
+            return "toindex";
+
+        } catch (UnknownAccountException e) {
+//            e.printStackTrace()
+//            登录失败:用户名不存在
+            model.addAttribute("loginMsg", "用户名不存在!");
+            return "tologin";
+
+        } catch (IncorrectCredentialsException e) {
+//            e.printStackTrace()
+            model.addAttribute("loginMsg", "密码错误!");
+            return "tologin";
         }
-      return "login";
+
     }
 }
