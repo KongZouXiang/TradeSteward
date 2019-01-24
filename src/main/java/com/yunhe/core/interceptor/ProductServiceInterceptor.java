e6aaac7b4d162a8f984f7269e0a37208d863ee81
271066981@qq.com <Kzx123>
2019-01-24 14:16:39 +0800

    增加Redis配置

---
+++ src/main/java/com/yunhe/core/interceptor/ProductServiceInterceptor.java
@@ -0,0 +1,42 @@
+package com.yunhe.core.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Map;
+
+@Component
+public class ProductServiceInterceptor implements HandlerInterceptor {
+
+    //在请求处理之前进行调用（Controller方法调用之前
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+
+        String method = httpServletRequest.getRequestURI();
+
+        System.out.println(method);
+        return true;
+        /*if(map.get("name").equals("zhangsan")) {
+            return true;    //如果false，停止流程，api被拦截
+        }else {
+            PrintWriter printWriter = httpServletResponse.getWriter();
+            printWriter.write("please login again!");
+            return false;
+        }*/
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle被调用");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("afterCompletion被调用");
+    }
+}
