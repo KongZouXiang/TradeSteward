7a8ded2c1bbf9dcceb3fc341f11f424dddd5998f
271066981@qq.com <Kzx123>
2019-01-26 17:56:44 +0800

    备份Sql

--- src/main/java/com/yunhe/config/QuartzConfig.java
+++ src/main/java/com/yunhe/config/QuartzConfig.java
@@ -6,6 +6,8 @@ import org.quartz.SchedulerException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 
 import java.io.IOException;
 
@@ -17,6 +19,33 @@ import java.io.IOException;
 @Configuration
 public class QuartzConfig {
 
+
+    /**
+     * 1.创建Job对象
+     */
+    @Bean
+    public JobDetailFactoryBean jobDetailFactoryBean() {
+        JobDetailFactoryBean factory = new JobDetailFactoryBean();
+        //关联我们自己的Job类
+        factory.setJobClass(LogJob.class);
+        return factory;
+    }
+
+    /**
+     * 2.创建Trigger对象 触发器，用于定义任务调度时间规则
+     */
+    @Bean
+    public SimpleTriggerFactoryBean simpleTriggerFactoryBean(JobDetailFactoryBean jobDetailFactoryBean) {
+        SimpleTriggerFactoryBean factory = new SimpleTriggerFactoryBean();
+        //关联JobDetail对象
+        factory.setJobDetail(jobDetailFactoryBean.getObject());
+        //该参数表示一个执行的毫秒数
+        factory.setRepeatInterval(2000);
+        //重复次数
+        factory.setRepeatCount(1);
+        return factory;
+    }
+
     /**
      * <p>
      * scheduler任务调度器
@@ -26,23 +55,12 @@ public class QuartzConfig {
      * @throws IOException
      * @throws SchedulerException
      */
-   /* @Bean
-    public Scheduler scheduler() throws IOException, SchedulerException {
-        SchedulerFactory schedulerFactory = new StdSchedulerFactory(quartzProperties());
-        Scheduler scheduler = schedulerFactory.getScheduler();
-        scheduler.start();
-        return scheduler;
-    }*/
-
-
-    /**
-     * 1.创建Job对象
-     */
     @Bean
-    public JobDetailFactoryBean jobDetailFactoryBean(){
-        JobDetailFactoryBean factory = new JobDetailFactoryBean();
-        //关联我们自己的Job类
-        factory.setJobClass(LogJob.class);
+    public SchedulerFactoryBean schedulerFactoryBean(SimpleTriggerFactoryBean simpleTriggerFactoryBean) {
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        //关联trigger
+        factory.setTriggers(simpleTriggerFactoryBean.getObject());
+
         return factory;
     }
 
--- src/main/java/com/yunhe/core/quartz/LogJob.java
+++ src/main/java/com/yunhe/core/quartz/LogJob.java
@@ -1,17 +1,25 @@
 package com.yunhe.core.quartz;
 
+import com.yunhe.systemsetup.dao.SystemLogMapper;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.stereotype.Component;
 
-import java.util.Date;
+import javax.annotation.Resource;
 
+@Component
 public class LogJob implements Job {
+
+    @Resource
+    SystemLogMapper systemLogMapper;
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 
+
+     /*   String convertString = RUDResult.ConvertString(systemLogMapper.delete(null), ResultRule.DELETE);
         // 执行响应的任务.
-        System.out.println("HelloJob.execute," + new Date());
+        System.out.println(convertString + new Date());*/
     }
 }
